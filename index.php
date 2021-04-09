6a591ac3e1c6d1454ae456cd989c43ec4f4aeb29
fazilhussain <fazilsultan51@gmail.com>
2021-04-09 10:42:41 +0500

    code

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/// Start Script 
+
+
+/// Dice 1
+$dice = rand(1,6);
+/// Dice 2
+$dice2 = rand(1,6);
+
+/// Result Dice 1
+echo $dice ;
+/// Result Dice 2
+ echo $dice2 ;
+
+
+ $result = $dice + $dice2;
+  /// Result of One through
+ echo $result;
+
+
+
+/// Script end
+?>
+
